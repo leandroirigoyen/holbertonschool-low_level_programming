9115aa625c5381184e25f086d0d2396ba2e9b2c7
root <leandro.irigoyen97@gmail.com>
2022-02-03 10:13:08 -0800

    Shell variables expansions

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,14 +4,19 @@
  * Return: el return tiene que dar 0
  */
 int main(void)
-{ int intType;
-  float floatType;
-  double doubleType;
-  char charType;
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
-printf("Size of char: %zu byte\n", sizeof(charType));
+{
+char charType;
+int intType;
+long int longintType;
+long long int longlongintType;
+float floatType;
+
+
+printf("Size of a char: %zu byte\n", sizeof(charType));
+printf("Size of an int: %zu bytes\n", sizeof(intType));
+printf("Size of a long int: %zu bytes\n", sizeof(longintType));
+printf("Size of a long long int: %zu bytes\n", sizeof(longlongintType));
+printf("Size of a float: %zu byte\n", sizeof(floatType));
 
 
 	return (0);
