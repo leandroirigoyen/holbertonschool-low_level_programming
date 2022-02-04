370075a601367bde71fc37ad0acaf073a1fb3063
root <leandro.irigoyen97@gmail.com>
2022-02-04 09:57:46 -0800

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main
+ * Return: algo
+ */
+int main(void)
+{
+int x;
+char sl = '\n';
+
+for (x = 'a'; x <= 'z'; x++)
+{
+if (x == q || x == e)
+}
+
+else
+{
+putchar(x);
+}
+
+putchar(sl);
+return (0);
+}
