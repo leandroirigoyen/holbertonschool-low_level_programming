c357af6594b0ca084cc8716214bb8d651327bef0
root <leandro.irigoyen97@gmail.com>
2022-02-04 09:24:02 -0800

    lowtoupper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,49 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- *  * main - main
- *   * Return: algo
- *    */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <ctype.h>
 /**
- *  * main - main
- *   * Return: algo
- *    */
-
+ * * main - main
+ * * Return: algo
+ */
 int main(void)
 {
+
 int x;
 char sl = '\n';
+
 for (x = 'a'; x <= 'z'; x++)
 {
 putchar(x);
 }
-putchar(sl);
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <ctype.h>
-/**
- *  * main - main
- *   * Return: algo
- *    */
 
-int main(void)
-{
-int x;
-char sl = '\n';
-for (x = 'a'; x <= 'z'; x++)
-{
-putchar(x);
-}
 for (x = 'A'; x <= 'Z'; x++)
-{	
-putchar(sl);
+{
+putchar(x);
 }
+
+putchar(sl);
 return (0);
+
 }
