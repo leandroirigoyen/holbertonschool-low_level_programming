dd51eb1cc7be9dfc39d5e826e7ac67f5ba720577
root <leandro.irigoyen97@gmail.com>
2022-02-04 07:54:33 -0800

    ifelse

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ lastDigit=n%10;
 
 if (lastDigit > 5)
 {
-printf("Last Digit of %d and is %d greater than 5\n", n, n%10);
+printf("Last Digit of %d is %d and is greater than 5\n", n, n%10);
 }
 else if (lastDigit == 0)
 {
