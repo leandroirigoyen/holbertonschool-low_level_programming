213f1ba6611d243093b45405eba40feaaf5297a5
root <leandro.irigoyen97@gmail.com>
2022-02-04 17:55:55 -0800

    printcomb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,18 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
 /**
  * main - main
- * Return:
+ * Return: algo
+ */
+int main(void)
 
-
-for (i = 0; i <= 7; i++)
 {
-    for (j = i+1; j <= 8; j++)
-    {
-        for (k = j+1; k <= 9; k++)
-        {
-            printf("%d%d%d\n", i, j, k);
-        }
-    }
+
+int i;
+int j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+
+
+putchar(i % 10 + '0');
+putchar(j % 10 + '0');
+
+if (!(i == 8 && j == 9))
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+}
+putchar('\n');
+return (0);
 }
