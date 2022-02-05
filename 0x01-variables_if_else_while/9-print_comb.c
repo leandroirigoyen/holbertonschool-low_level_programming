b430e3447def1ca3ef144c678a269e8a6920a373
root <leandro.irigoyen97@gmail.com>
2022-02-04 18:29:58 -0800

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<time.h>
+#include<stdlib#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
 /**
- * main - main
- * Return: algo
- */
-
+ *  * main - main
+ *   * Return: algo
+ *    */
 int main(void)
+	
 {
-int n;
 
-for (n = 0; n < 10; n++)
+int i;
+
+for (i = 0; i < 10; i++)
 {
-putchar(n);
-if (n < 9)
+
+putchar(i % 10 + '0');
+
+if (i < 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
+
 putchar('\n');
 return (0);
 }
