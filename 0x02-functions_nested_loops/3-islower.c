5f1452e1504d2e89c32eabdc76aba7d79774edf1
root <leandro.irigoyen97@gmail.com>
2022-02-07 08:31:14 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,14 @@
-#include <stdlib.h>
-#include "_putchar.c"
-#include <ctype.h>
+#include "main.h"
+
 /**
   * main - main
   * Return: 1 if is lowercase if it's not print 0
 */
-int main(void)
+
+int _islower(int c)
 {
 
-
-/**
- * _islower - _islower
- * Return: 1
- */
-int _islower(int c)
-
-
-if (islower(c))
+if (_islower(c))
 {
 return (1);
 }
@@ -25,5 +17,4 @@ else
 return (0);
 }
 
-return (0);
 }
