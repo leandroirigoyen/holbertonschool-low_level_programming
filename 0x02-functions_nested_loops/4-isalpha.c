90de98e75454135c783c84a88fb9a639133073cd
root <leandro.irigoyen97@gmail.com>
2022-02-07 08:54:52 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - _isalpha
+ * @c: letter, lowercase or uppercase
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+
+if (isalpha(c))
+{
+_putchar(c);
+}
+else
+{
+return (0);
+}
+
+
+return (0);
+}
