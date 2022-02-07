45da130fed9c07c135f8dc42bfdf4e1d95f49af3
root <leandro.irigoyen97@gmail.com>
2022-02-07 10:31:38 -0800

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,17 @@
 
 int print_last_digit(int n)
 {
-_putchar(n);	
+if (n > 0)
+{	
+_putchar((n % 10) + '0');	
 return (n % 10);
+}
+else
+{
+_putchar (((n * -1) % 10) + '0');
+return ((n * -1) % 10);
+}
+
+
+return (0);
 }
