572172d06fa0722f9e3c08c589efc95a07ce9aa3
root <leandro.irigoyen97@gmail.com>
2022-02-07 08:12:21 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 char ch;
 
@@ -17,5 +17,4 @@ _putchar(ch);
 
 _putchar('\n');
 
-return (0);
 }
