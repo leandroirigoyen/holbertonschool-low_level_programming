9a02e916edcf1e93f5a2d47662e5909ecccd2f09
root <leandro.irigoyen97@gmail.com>
2022-02-07 07:48:55 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,6 @@
-#include "main.h"
 #include <unistd.h>
-#include "_putchar.c"
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+#include "main.h"
+
 /**
  * main - prints _putchar
  * Return: algo
@@ -18,13 +11,7 @@ char word[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n'};
 int i;
 
 for (i = 0; i < 9; i++)
-{
 _putchar(word[i]);
-
-}
-
-
-
 
 return (0);
 }
