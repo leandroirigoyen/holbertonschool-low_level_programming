0acab35a280c208b9649ba816d00557da6a06e55
root <leandro.irigoyen97@gmail.com>
2022-02-07 10:01:33 -0800

    lastdigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - imprimir ultimo digito
+ * @n: var
+ * Return: value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+print_last_digit(n);
+return (n);
+}
