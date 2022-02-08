08f13be125dd8f979302eac1d6ac1ed3c6d6c2e6
root <leandro.irigoyen97@gmail.com>
2022-02-07 16:57:47 -0800

    timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,30 +10,37 @@ int j;
 int i;
 int k;
 
-for (i = 0; i < 10; i++)
-	{
-	for (j = 0; j < 10; j++)
-		{
-			k = j * i;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
+i = 0;
+while (i < 10)
+{
+        j = 0;
+        while (j < 10)
+        {
+                k = i *j;
 
-			if (k < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+                if ((k / 10) + '0' != '0')
+                {
+                        _putchar((k / 10) + '0');
+                } else
+                {
+                        if (j != 0)
+                        {
+                                _putchar(' ');
+                        }
+                }
+
+                _putchar((k % 10) + '0');
+
+                if (j != 9)
+                {
+                        _putchar(',');
+                        _putchar(' ');
+                }
+
+                j++;
+        }
+        _putchar('\n');
+        i++;
+
 }
+-- INSERT --
