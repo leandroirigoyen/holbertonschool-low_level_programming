76103e9092bb26b2cf83f4415506831aab0c1136
root <leandro.irigoyen97@gmail.com>
2022-02-09 07:23:48 -0800

    functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies
+ * @a: var a
+ * @b: var b
+ * Return: r
+ */
+int mul(int a, int b)
+{
+int r = a * b;
+
+return (r);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print_numbers
+ * Return: 0
+ */
+void print_numbers(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
