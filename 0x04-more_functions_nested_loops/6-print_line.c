7d9291257ac84476b54dd46a765d628d53248b66
root <leandro.irigoyen97@gmail.com>
2022-02-09 07:47:56 -0800

    functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line -print_line
+ * @n: var
+ * Return: 0
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+if (n <= 0)
+{
+_putchar('\n');
+}
+}
+}
