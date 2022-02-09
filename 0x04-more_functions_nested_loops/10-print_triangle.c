8e6eead9cd41f168d8575dd22789e247492e6965
root <leandro.irigoyen97@gmail.com>
2022-02-09 10:06:54 -0800

    functions

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,7 +8,9 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
-
+	
+	if (size != 0)
+	{	
 	for (i = 1; i <= size; i++)
 	{
 		for (j = i; j < size; j++)
@@ -20,5 +22,10 @@ void print_triangle(int size)
 			_putchar('#');
 		}
 			_putchar('\n');
+	}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
