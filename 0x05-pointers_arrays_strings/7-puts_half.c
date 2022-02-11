9dafa262723400b3b532db74f9c878cd396d04e8
root <leandro.irigoyen97@gmail.com>
2022-02-11 13:18:31 -0800

    Shell variables expansions

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,8 +7,23 @@
  */
 void puts_half(char *str)
 {
+int length;
+int mid;
+int i;
+int k;
 
+length = strlen(str);
+mid = length / 2;
 
+for (i = 0; i < mid; i++)
+{
+leftHalf[i] = str[i];
+}
+leftHalf[i] = '\0';
 
-
+for (i = mid, k = 0; i <= length; i++, k++)
+{
+	rightHalf[k] = str[i];
+}
+_putchar(%s\n);
 }
