07d3c316fc2d7f039d5023ecb87455ab5419da46
root <leandro.irigoyen97@gmail.com>
2022-02-11 09:44:25 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints reverse
+ * @s: pointer
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+int l;
+l = strlen((s) - 1);
+_putchar(l);
+}
