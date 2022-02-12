f11097aee97a0e78fc3db4068b5d0123a97a9b74
root <leandro.irigoyen97@gmail.com>
2022-02-11 17:10:04 -0800

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,19 +8,14 @@
  */
 void print_rev(char *s)
 {
+	int i;
+	int len;
 
-int i;
-int len;
-int temp;
+	len = strlen(s);
 
-len = strlen(s);
-
-	for (i = 0; i < len / 2; i++)
+	for (i = len - 1; i >= 0; i--)
 	{
-		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
-
