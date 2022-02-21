f715bbeae24469ce0c1f7f05d91259ef0cd701a1
sambi0197 <leandro.irigoyen97@gmail.com>
2022-02-21 09:05:49 -0800

    pointersandarrays

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strpbkr - strpbkr
+ * @s: string s
+ * @accept: string accept
+ * Return: pointer to byte in s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char s1[s] = "%c"
+	char s2[accept] = "%c"
+
+	char *result;
+
+	result = strpbrk(s, accept);
+
+	return(0);
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - strstr
+ * @haystack: string
+ * @needle: substring
+ * Return: Pointer or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *begin = haystack;
+	char *pattern = needle;
+
+	while (*haystack && *pattern && *haystack == *pattern)
+	{
+		haystack++;
+		pattern++;
+	}
+	return begin + 1;
+}
