e91e4c646698e4373d6f49da274ab4e41f2a5018
sambi0197 <leandro.irigoyen97@gmail.com>
2022-02-21 18:10:57 -0800

    pointersandarrays

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,14 +7,19 @@
  * Return: Pointer or null
  */
 char *_strstr(char *haystack, char *needle)
-{
-	char *begin = haystack;
-	char *pattern = needle;
 
-	while (*haystack && *pattern && *haystack == *pattern)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		haystack++;
-		pattern++;
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
 	}
-	return (begin + 1);
+	return (NULL);
 }
