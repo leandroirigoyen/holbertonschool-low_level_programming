075e735e6eea13b211433c92fd244f12a58bfb75
sambi0197 <leandro.irigoyen97@gmail.com>
2022-02-23 14:45:39 -0800

    recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "math.h"
+/**
+ * _pow_recursion(int x, int y)
+ * @x: intx
+ * @y: inty
+ * Return: 0
+ */
+int _pow_recursion(int x, int y)
+{
+	int resultado;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+		return (1);
+	
+	else
+	{
+		
+		resultado = x * _pow_recursion(x, (y - 1));
+		
+		return (resultado);
+	}
+
+return (0);
+}
