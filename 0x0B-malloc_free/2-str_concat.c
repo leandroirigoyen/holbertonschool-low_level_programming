894e62826e699f1473fe89de2240fe413074c00c
sambi0197 <leandro.irigoyen97@gmail.com>
2022-03-02 05:45:53 -0800

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,17 @@ char *str_concat(char *s1, char *s2)
 	int p;
 	int q = 0;
 
-	rtn = malloc(strlen(s1) + strlen(s2) +1);
+	rtn = malloc(strlen(s1) + strlen(s2) + 1);
 
-	
-	for (p = 0; (rtn[q] = s1[p]) != '\0'; ++p, ++q) {}
-  	for (p = 0; (rtn[q] = s2[p]) != '\0'; ++p, ++q) {}
-  return (rtn);
+	if (rtn == NULL)
+		return ("");
+
+	for (p = 0; (rtn[q] = s1[p]) != '\0'; ++p, ++q)
+	{
+	}
+	for (p = 0; (rtn[q] = s2[p]) != '\0'; ++p, ++q)
+	{
+	}
+return (rtn);
 
 }
