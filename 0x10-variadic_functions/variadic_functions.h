035e32eac675e1b9af88fc281abd6920e33b0bea
sambi0197 <leandro.irigoyen97@gmail.com>
2022-03-10 18:05:02 -0800

    variadic

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,12 @@
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-void print_all(const char * const format, ...);
 
+#endif
