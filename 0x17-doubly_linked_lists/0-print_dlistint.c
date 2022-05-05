ec57bcc7fefba60f86d24433262126af2f0fdc4e
sambi0197 <leandro.irigoyen97@gmail.com>
2022-05-05 14:40:23 -0700

    dlinkedlists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ * print_dlistint - print double linked list
+ * @h: constant pointer linked list
+ * Return: nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
